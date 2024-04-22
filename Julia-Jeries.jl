c14954f38367bf889f00201035643e0a9b2396e2
Salwa Jeries <annette.jeries@gmail.com>
2024-04-21 20:45:47 -0500

    Added comments

--- Julia-Jeries.jl
+++ Julia-Jeries.jl
@@ -1,3 +1,39 @@
+#=
+  Assignment: Julia Program
+  Course: CS 424-02
+  Instructor: Professor Harry Delugach
+
+  Student: Salwa Jeries
+  Date: April 21, 2024
+
+  Summary:
+    This program reads in a "register.txt" file containing information about athletes and events, processes
+    the relationships between them, then outputs the following information:
+
+      - Each athlete's name, and the events they are participating in
+      - Each event's title, and the athletes that are participating in it
+
+    For the program design, I created an "Athlete" struct containing their ID, name, and a vector of corresponding
+    event IDs to the events they were participating in. I also created an "Event" struct containing its ID, title,
+    and a vector of corresponding athlete IDs to the athletes participating in the event. I created separate getter
+    and setter functions for these structs, which would take in the struct to perform the desired operations with.
+
+    The final two structs I made were the "AthleteList" and "EventList" structs, which contained vectors for their
+    respective struct elements as well as some separate special functions. These two structs made managing all the
+    athlete and event struct instances much easier. I also added some search functions that would find specific athlete
+    or event elements by ID from the vectors in the class. 
+
+    For the program flow, I followed the same general structure that I used in the original Ruby program assignemnt.
+    Since the input file was divided into three sections, I created three different file read sections to handle
+    the sets of data properly. The first section initialized each "Athlete" in the "AthleteList" struct, while the
+    second section initialized each "Event" in the "EventList" struct. The third section would search for the
+    "Athlete" struct with a matching ID to the file read in the "AthleteList" struct and assign it to the "Event"
+    struct with a matching ID to the file read in the "EventList" struct. Similarly, it would assign the "Event"
+    struct with a matching ID to the file read in the "EventList" struct to the "Athlete" struct with a matching
+    ID to the file read in the "AthleteList" struct.
+
+=#
+
 mutable struct Athlete
   athlete_id::String
   name::String
@@ -186,3 +222,23 @@ for event in get_list(all_events)
   end
   println()
 end
+
+#=
+  Thoughts and Impressions:
+    Once again, I found Julia to be relatively easy to pickup. It follows a similar style to Python and
+    Ruby by using the implicit types and no semicolons for the end of line marker. It was interesting that
+    Julia uses the same "end" marker for loops and conditional statements and the same print statements as
+    Ruby. Another interesting observation was the explicit "global" variable reference to ensure that a loop
+    or function knew whether it was referring to a local or global variable. This hindered writability slightly
+    (although not by much), but helped with readability so that it was an explicit reminder of the scope of
+    said variable.
+
+    Not having objects meant that I had to change the structure of the Athlete and Event "elements" in the code
+    compared to the previous assignment, however it was relatively straightforward. The structs were easy to
+    define, and I used separate functions that would be passed an Athlete/Event struct to perform operations with
+    it. Otherwise, the loss of objects had no affect on the program and I did not have to think of a different
+    logic for performing the requirements of the assignment. Just based on the language itself and not based on
+    the specific project/use case in which I would need to use these langauges, I would still pick Ruby over Julia.
+    This is primarily because I enjoy the object-oriented features in Ruby, which are lost in Julia. Otherwise, they
+    feel pretty similar and I would be willing to work with either one.
+=#
